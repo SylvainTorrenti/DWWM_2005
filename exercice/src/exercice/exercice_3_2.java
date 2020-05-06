51dd1e404fb4173f7c2f9647349ddac848238748
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-05-06 15:05:55 +0200

    fhngh

---
+++ exercice/src/exercice/exercice_3_2.java
@@ -0,0 +1,10 @@
+package exercice;
+
+public class exercice_3_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ exercice/src/exercice/exercie_3_1.java
@@ -0,0 +1,10 @@
+package exercice;
+
+public class exercie_3_1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
