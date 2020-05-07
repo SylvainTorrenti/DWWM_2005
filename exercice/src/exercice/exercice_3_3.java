76c214af235bb94b65c24195440f3e4fbddcd2f9
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-05-07 14:31:29 +0200

    ajout de la condition 120 char

--- exercice/src/exercice/exercice_3_3.java
+++ exercice/src/exercice/exercice_3_3.java
@@ -9,265 +9,270 @@ public class exercice_3_3 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Entrez un texte de 120 caractère :");
 		String texte = sc.nextLine();
-		int compte = 0;
-		for (char lettre : texte.toCharArray()) {
+		if (texte.length() != 120) {
+			System.out.println("Le texte ne fait pas 120 caractère.");
+		} else {
+			int compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-			if (lettre == 'a' || lettre == 'A') {
-				compte++;
+				if (lettre == 'a' || lettre == 'A') {
+					compte++;
 
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre a");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'b' || lettre == 'B') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre a");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre b");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'b' || lettre == 'B') {
+					compte++;
 
-			if (lettre == 'c' || lettre == 'C') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre c");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'd' || lettre == 'D') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre b");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre d");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'c' || lettre == 'C') {
+					compte++;
 
-			if (lettre == 'e' || lettre == 'E') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre e");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'f' || lettre == 'F') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre c");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre f");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'd' || lettre == 'D') {
+					compte++;
 
-			if (lettre == 'g' || lettre == 'G') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre g");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'h' || lettre == 'H') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre d");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre h");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'e' || lettre == 'E') {
+					compte++;
 
-			if (lettre == 'i' || lettre == 'I') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre i");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'j' || lettre == 'J') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre e");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre j");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'f' || lettre == 'F') {
+					compte++;
 
-			if (lettre == 'k' || lettre == 'K') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre k");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'l' || lettre == 'L') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre f");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre l");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'g' || lettre == 'G') {
+					compte++;
 
-			if (lettre == 'm' || lettre == 'M') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre m");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'n' || lettre == 'N') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre g");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre n");
-		compte = 0;for (char lettre : texte.toCharArray()) {
+				if (lettre == 'h' || lettre == 'H') {
+					compte++;
 
-			if (lettre == 'o' || lettre == 'O') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre o");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'p' || lettre == 'P') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre h");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre p");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'i' || lettre == 'I') {
+					compte++;
 
-			if (lettre == 'q' || lettre == 'Q') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre q");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'r' || lettre == 'R') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre i");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre r");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'j' || lettre == 'J') {
+					compte++;
 
-			if (lettre == 's' || lettre == 'S') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre s");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 't' || lettre == 'T') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre j");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre t");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'k' || lettre == 'K') {
+					compte++;
 
-			if (lettre == 'u' || lettre == 'U') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre u");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'v' || lettre == 'V') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre k");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre v");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'l' || lettre == 'L') {
+					compte++;
 
-			if (lettre == 'w' || lettre == 'W') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre w");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'x' || lettre == 'X') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre l");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
-		}
-		System.out.println("Il y a " + compte + " fois la lettre x");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
+				if (lettre == 'm' || lettre == 'M') {
+					compte++;
 
-			if (lettre == 'y' || lettre == 'Y') {
-				compte++;
-
-			}
-
-		}
-		System.out.println("Il y a " + compte + " fois la lettre y");
-		compte = 0;
-		for (char lettre : texte.toCharArray()) {
-
-			if (lettre == 'z' || lettre == 'Z') {
-				compte++;
+				}
 
 			}
+			System.out.println("Il y a " + compte + " fois la lettre m");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
 
+				if (lettre == 'n' || lettre == 'N') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre n");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'o' || lettre == 'O') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre o");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'p' || lettre == 'P') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre p");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'q' || lettre == 'Q') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre q");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'r' || lettre == 'R') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre r");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 's' || lettre == 'S') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre s");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 't' || lettre == 'T') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre t");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'u' || lettre == 'U') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre u");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'v' || lettre == 'V') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre v");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'w' || lettre == 'W') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre w");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'x' || lettre == 'X') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre x");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'y' || lettre == 'Y') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre y");
+			compte = 0;
+			for (char lettre : texte.toCharArray()) {
+
+				if (lettre == 'z' || lettre == 'Z') {
+					compte++;
+
+				}
+
+			}
+			System.out.println("Il y a " + compte + " fois la lettre z");
+			compte = 0;
 		}
-		System.out.println("Il y a " + compte + " fois la lettre z");
-		compte = 0;
 	}
 }
