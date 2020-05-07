0b9b60558a081956eefa2d462238565d4fc4d84d
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-05-07 16:13:24 +0200

    ajout exo 4

--- exercice/src/exercice/exercice_3_4.java
+++ exercice/src/exercice/exercice_3_4.java
@@ -1,10 +1,15 @@
 package exercice;
+import java.util.Arrays;
 
 public class exercice_3_4 {
 
 	public static void main(String[] args) {
-		
-
+		int[] tab1 = { 2, 5, 4, 8, 7 };
+		Arrays.sort(tab1); 
+		  
+        for (int i = 0; i < tab1.length; i++) {
+			System.out.println(tab1[i]);
+		}
 	}
 
 }
