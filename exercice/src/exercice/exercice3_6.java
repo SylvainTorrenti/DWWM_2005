ecb78027f370a0a30fe4fc99049efed239fae619
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-05-11 15:20:00 +0200

    debut exo 3.6

--- exercice/src/exercice/exercice3_6.java
+++ exercice/src/exercice/exercice3_6.java
@@ -1,4 +1,7 @@
 package exercice;
+
+import java.util.Scanner;
+
 /**
  * @author Sylvain torrenti
  * @version 1.0
@@ -7,8 +10,25 @@ package exercice;
 public class exercice3_6 {
 
 	public static void main(String[] args) {
+		int essai = 0;
+		int i;
+		char devine;
+		System.out.println("Entrez un mot d'au moins 5 caractéres:");
+		Scanner sc = new Scanner(System.in);
+		String mot = sc.nextLine();
+		char[] reponse = mot.toCharArray();
+
+		for (i = 0; i < reponse.length; i++) {
+			reponse[i] = '-';
+			System.out.print(reponse[i]);
+		}
 		
+		System.out.println("\n"+"Entrez la lettre:");
+		devine = sc.next().charAt(0);
+		if (mot.charAt(i) == devine) {
+			reponse[i] = devine;
+
+		}
 
 	}
-
 }
