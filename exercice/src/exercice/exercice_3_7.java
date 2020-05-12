8984604666483c52a4661924359d11f9d622bc29
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-05-12 11:29:14 +0200

    exo 3.7

---
+++ exercice/src/exercice/exercice_3_7.java
@@ -0,0 +1,31 @@
+package exercice;
+
+import java.util.Scanner;
+
+/**
+ * @author Sylvain torrenti
+ * @version 1.0
+ * @since 11/05/20
+ */
+public class exercice_3_7 {
+
+	public static void main(String[] args) {
+		boolean present = false;
+		Scanner sc = new Scanner(System.in);
+		String[] nom = { "agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie" };
+		System.out.println("Entrez un prénom:");
+		String nom1 = sc.nextLine();
+		for (int i = 0; i < nom.length; i++) {
+			if (nom[i].equalsIgnoreCase(nom1)) {
+				System.out.println(i + 1);
+				present = true;
+				break;
+			}
+
+		}
+		if (present == false) {
+			System.out.println("Le nom n'est pas présent!");
+		}
+	}
+
+}
