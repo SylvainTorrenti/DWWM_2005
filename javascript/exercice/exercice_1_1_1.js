ea337d970c2a7a6ea7b005b434e8eae3834c02cb
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-05-19 13:48:07 +0200

    opti exercie 1.1.1

--- javascript/exercice/exercice_1_1_1.js
+++ javascript/exercice/exercice_1_1_1.js
@@ -1,18 +1,15 @@
 const readline = require("readline");
-
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
-
-rl.question("Entrez un nombre" + "\n", (answer) => {
-
-    var nombre_1 = answer;
-    rl.question("Entre un 2eme nombre" + "\n", (answer2) => {
-        var nombre_2 = answer2;
-
-        let moyenne = (parseInt(nombre_1) + parseInt(nombre_2)) / 2;
-        console.log(moyenne);
-    });
-
-});
+rl.question(
+  "\n" + "Saisir deux nombres séparés pas un espace :" + "\n",
+  (answer) => {
+    let nb = answer.split(" ");
+    nb[0] = parseFloat(nb[0]);
+    nb[1] = parseFloat(nb[1]);
+    let moyenne = (nb[0] + nb[1]) / 2;
+    console.log("La moyenne est de : " + moyenne);
+  }
+);
