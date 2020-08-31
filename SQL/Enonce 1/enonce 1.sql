4e3b403bf14a7bab74dac7d70f7ab30d0202a95c
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-08-31 09:28:34 +0200

    Exercice SQL / Enonce 1 update

--- SQL/Enonce 1/enonce 1.sql
+++ SQL/Enonce 1/enonce 1.sql
@@ -5,24 +5,24 @@ use enonce_1;
 
 drop table if exists personnes;
 CREATE TABLE IF NOT EXISTS personnes (
-    personnes_id INT(11) NOT NULL PRIMARY KEY,
+    personnes_id INT NOT NULL PRIMARY KEY,
     personnes_nom VARCHAR(50) NOT NULL,
     personnes_prenom VARCHAR(50) NOT NULL,
-    personnes_no_rue INT(3) NOT NULL,
+    personnes_no_rue TINYINT NOT NULL,
     personnes_rue VARCHAR(100) NOT NULL,
-    personnes_cp INT(5) NOT NULL,
+    personnes_cp TINYINT NOT NULL,
     personnes_ville VARCHAR(50) NOT NULL
 )  ENGINE=INNODB CHARSET UTF8 COLLATE UTF8_GENERAL_CI;
 
 drop table if exists vehicule;
 CREATE TABLE IF NOT EXISTS vehicule (
-    personnes_id INT(11) NOT NULL,
+    personnes_id INT NOT NULL,
     vehicule_immatriculation VARCHAR(7) NOT NULL,
     vehicule_marque VARCHAR(10) NOT NULL,
-    vehicule_kilométrage INT(6) NOT NULL,
+    vehicule_kilométrage INT NOT NULL,
     vehicule_date_mise_en_service DATE NOT NULL,
-    PRIMARY KEY (personnes_id , vehicule_immatriculation),
-    FOREIGN KEY (personnes_id)
+    CONSTRAINT pk_vehicule PRIMARY KEY (vehicule_immatriculation),
+    CONSTRAINT fk_personne FOREIGN KEY (personnes_id)
         REFERENCES personnes (personnes_id)
 )  ENGINE=INNODB CHARSET UTF8 COLLATE UTF8_GENERAL_CI;
     
