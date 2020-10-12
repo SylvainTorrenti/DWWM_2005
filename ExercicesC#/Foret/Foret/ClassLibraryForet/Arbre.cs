6219dab3a94819d2981ecfb53d07f91e28d977ba
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-10-12 13:31:47 +0200

    foret

---
+++ ExercicesC#/Foret/Foret/ClassLibraryForet/Arbre.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForet
+{
+    public class Arbre
+    {
+        int hauteur;
+        private List<Feuille> feuilles;
+
+        public Arbre(int hauteur)
+        {
+            this.hauteur = hauteur;
+            feuilles = new List<Feuille>();
+
+        }
+
+        public Arbre(int hauteur, List<Feuille> _feuilles) //ouvre porte aggregation
+        {
+            this.hauteur = hauteur;
+            feuilles = _feuilles;
+        }
+
+        public void AddFeuille(Feuille f)
+        {
+            this.feuilles.Add(f);
+        }
+
+        public int Hauteur
+        {
+            get => hauteur;
+            //set => hauteur = value; 
+        }
+
+        public override string ToString()
+        {
+            string str = base.ToString() + "\nHauteur : " + hauteur + "M";
+
+            if (this.feuilles.Count() > 0)
+            {
+                str += "\nFeuilles :";
+                foreach (Feuille f in feuilles)
+                {
+                    str += "\n" + f.ToString();
+                }
+            }
+            
+            return str;
+        }
+    }
+}
---
+++ ExercicesC#/Foret/Foret/ConsoleAppTestForet/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibraryForet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestForet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Arbre arbre1 = new Arbre(5);
+            Feuille feuille1 = new Feuille(5, EnumCouleur.Vert, EnumFormeFeuille.triangulaire);
+            Feuille feuille2 = new Feuille(6, EnumCouleur.Vert, EnumFormeFeuille.triangulaire);
+            Feuille feuille3 = new Feuille(7, EnumCouleur.Vert, EnumFormeFeuille.triangulaire);
+
+            arbre1.AddFeuille(feuille1);
+            arbre1.AddFeuille(feuille2);
+            arbre1.AddFeuille(feuille3);
+
+            Console.WriteLine(arbre1.ToString());
+            Console.ReadLine();
+        }
+    }
+}
