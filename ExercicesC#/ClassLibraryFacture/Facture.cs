1fb71fe5da1502e5674bc32cb648e8c4c58f22a4
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-10-26 12:15:36 +0100

    maj validation

---
+++ ExercicesC#/ClassLibraryFacture/Facture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryFacture
+{
+    public class Facture
+    {
+
+        private string nom;
+        private DateTime date;
+        private float montant;
+        private string codePostal;
+
+        public Facture()
+        {
+            nom = "";
+            date = DateTime.Now;
+            montant = 0;
+            codePostal = "00000";
+        }
+        public Facture(string _nom, DateTime _date, float _montant, string _codePostal)
+        {
+            this.nom = _nom;
+            this.date = _date;
+            this.montant = _montant;
+            this.codePostal = _codePostal;
+
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public float Montant { get => montant; set => montant = value; }
+        public string CodePostal { get => codePostal; set => codePostal = value; }
+
+        public override string ToString()
+        {
+            return
+                $"Name : {nom}\n" +
+                $"Date : {date:dd/MM/yyyy}\n" +
+                $"Amount : {Math.Round(montant, 2)}\n" +
+                $"Zip Code : {codePostal}";
+        }
+    }
+}
---
+++ ExercicesC#/ClassLibraryVerification/Verification.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibraryVerification
+{
+    public static class Verification
+    {
+
+        private const string regexNom = @"^[A-Za-z]+$";
+        private const string regexCp = @"^(?:[0-8]\d|[0-8])\d{3}$";
+
+
+
+        public static bool ValidDate(string _date)
+        {
+            if (_date.Length != 10)
+            {
+                return false;
+            }
+            DateTime factureDate = new DateTime();
+            if (!DateTime.TryParseExact(_date, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.AssumeLocal, out factureDate))
+            {
+                return false;
+            }
+            if (factureDate <= DateTime.Today)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool ValidNom(string _nom)
+        {
+            return Regex.IsMatch(_nom, regexNom);
+
+        }
+
+        public static bool ValidMontant(string _montant)
+        {
+
+            if (float.TryParse(_montant, out float result))
+            {
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static bool ValidCP(string _cp)
+        {
+            return Regex.IsMatch(_cp, regexCp);
+
+        }
+        public static void ErreurSaisie(TextBox textbox)
+        {
+            textbox.Focus();
+            SystemSounds.Exclamation.Play();
+        }
+
+    }
+
+
+}
