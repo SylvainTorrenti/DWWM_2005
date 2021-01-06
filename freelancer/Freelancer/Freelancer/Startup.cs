b4ae6ed9fd552b7408c6da7b7aa28d81440bc84c
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2021-01-06 17:49:46 +0100

    freelance

--- freelancer/Freelancer/Freelancer/Startup.cs
+++ freelancer/Freelancer/Freelancer/Startup.cs
@@ -16,6 +16,7 @@ namespace Freelancer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +30,8 @@ namespace Freelancer
 
             app.UseStaticFiles();
 
+            app.UseRouting();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
@@ -37,15 +40,6 @@ namespace Freelancer
                     );
             });
 
-            /*app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });*/
         }
     }
 }
