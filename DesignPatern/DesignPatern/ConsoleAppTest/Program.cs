812593d8f4778ac0b42b2cc144efa7b62e1aac48
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2021-01-26 07:14:24 +0100

    Preparation figure S

--- DesignPatern/DesignPatern/ConsoleAppTest/Program.cs
+++ DesignPatern/DesignPatern/ConsoleAppTest/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
+            Expression e = new Nombre(33);
+            string strnumber= e.Formate();
+
             Expression exp1 = new Addition(new Nombre(33), new Nombre(33));
             int res = exp1.Evalue();
 
@@ -16,7 +19,7 @@ namespace ConsoleAppTest
             Expression exp3 = new Soustraction(new Nombre(3), new Nombre(6));
             int res3 = exp3.Evalue();
 
-            Expression exp4 = new Addition(new Soustraction(new Nombre(3), new Nombre(6)),new Nombre(7));
+            Expression exp4 = new Addition(new Addition(new Nombre(3), new Nombre(6)),new Nombre(7));
             int res4 = exp4.Evalue();
             string str4 = exp4.Formate();
 
